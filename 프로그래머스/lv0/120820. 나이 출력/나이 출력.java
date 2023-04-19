8fbd426a26fd07c49cdaea1f3582cd7a612a9cbe
한승록 <127282898+shl2587@users.noreply.github.com>
2023-04-20 00:12:15 +0900

    Update 나이 출력.java

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int age) {
-        int answer = 2022 - age + 1;
+        int answer = 2022 - age + 1;    // 기준 연도에서 매개변수로 받은 연도를 빼준뒤 1을 더해주면 나이가 계산됨
         return answer;
     }
 }
