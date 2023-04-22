89e13bd1d9fdaf80a97b04f8a4ee8142830ce38f
한승록 <127282898+shl2587@users.noreply.github.com>
2023-04-22 12:53:12 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        double answer = num1 / (double)num2 * 1000;
-        return (int)answer;
+        double answer = num1 / (double)num2 * 1000; // 나눗셈을 진행할때 소수점 아래 수를 구하려면 실수로 계산해야함
+                                                    // 실수로 나눠주기위한 형변환
+        return (int)answer;                         // 정수부분만 반환하기 위한 형변환
     }
 }
