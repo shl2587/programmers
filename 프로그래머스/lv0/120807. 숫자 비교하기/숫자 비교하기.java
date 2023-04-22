31cbae286744819597344fc9c2625923d08da8fb
한승록 <127282898+shl2587@users.noreply.github.com>
2023-04-22 12:46:49 +0900

    Update 숫자 비교하기.java

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        return num1 == num2 ? 1 : -1;
+        return num1 == num2 ? 1 : -1;   // 삼항연산자를 통하여 두 수가 같으면 1을
+                                        // 다르면 -1을 반환
     }
 }
