97249b98da89aac808d41f9fdde1647a90be50cb
한승록 <127282898+shl2587@users.noreply.github.com>
2023-05-01 20:44:08 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 2.40 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int k) {
-        int[] answer = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            if (k % 2 == 0) {
-                answer[i] = arr[i] + k;    
+        int[] answer = new int[arr.length]; // 정답을 담을 answer배열 선언 및 크기 설정
+        for (int i = 0; i < arr.length; i++) {  // 배열의 길이로 반복회수 제어
+            if (k % 2 == 0) {                   // 매개변수 k가 짝수라면
+                answer[i] = arr[i] + k;         // 각 요소에 k를 더하여 answer배열에 저장
             }
-            else {
-                answer[i] = arr[i] * k;
+            else {                              // 홀수라면
+                answer[i] = arr[i] * k;         // 각 요소에 k를 곱하여  answer배열에 저장
             }
         }
         
