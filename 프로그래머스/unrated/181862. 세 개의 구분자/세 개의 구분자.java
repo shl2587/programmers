2d56e03591940b79245d2c9f2f1e85874f4c6305
한승록 <127282898+shl2587@users.noreply.github.com>
2023-05-21 16:46:31 +0900

    [unrated] Title: 세 개의 구분자, Time: 124.81 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,23 +3,32 @@ import java.util.*;
 class Solution {
     public ArrayList<String> solution(String myStr) {
         ArrayList<String> list = new ArrayList<>();
-        String result = "";
+        String result = ""; // 나눈 문자열을 저장할 result 문자열 변수 선언
         for (int i = 0; i < myStr.length(); i++) {
-            if (!('a' <= myStr.charAt(i) && myStr.charAt(i) <= 'c')) {
+            // 해당 열의 문자열이 a, b, c 중 하나가 아닐 때 
+            if (!('a' <= myStr.charAt(i) && myStr.charAt(i) <= 'c')) { 
+                // result에 해당 문자열을 더해줌
             	result += myStr.charAt(i);            	
             }
+            // 해당 열의 문자열이 a, b, c 중 하나가 아니면서 빈문자열이 아니면
             else if (!result.equals("")) {
-            	list.add(result);
+            	// 해당 list에 result 문자열 추가
+                list.add(result); 
             }
+            // a, b, c 중에 하나면
             if ('a' <= myStr.charAt(i) && myStr.charAt(i) <= 'c') {
-            	result = "";
+            	// 빈문자열 저장
+                result = "";
             }
         }
 
-        
+        // list 크기가 0이고 result의 길이가 1이 아니면
+        // 중복되는 문자열이 없다는 의미이므로
         if (list.size() == 0 && result.length() != 1) {
+            // list에 EMPTY를 추가
             list.add("EMPTY");    
         }
+        // list에 result 추가
         else {
         	list.add(result);
         }
