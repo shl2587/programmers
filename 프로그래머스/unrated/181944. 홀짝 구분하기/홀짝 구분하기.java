7d6de3e57ea26d8d7f7feb142da99a9241f5d7d3
한승록 <127282898+shl2587@users.noreply.github.com>
2023-05-23 20:27:48 +0900

    [unrated] Title: 홀짝 구분하기, Time: 147.43 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);    // scanner클래스를 통해 값을 입력받고
+        int n = sc.nextInt();   // 정수 n에 값을 입력받고
         
-        if (n % 2 == 0) {
-            System.out.println(n + " is even");
+        if (n % 2 == 0) {   // n이 짝수이면
+            System.out.println(n + " is even"); // is even을 출력
         }
-        else {
-            System.out.println(n + " is odd");
+        else {  // 홀수이면
+            System.out.println(n + " is odd");  // is odd를 출력
         }
     }
 }
