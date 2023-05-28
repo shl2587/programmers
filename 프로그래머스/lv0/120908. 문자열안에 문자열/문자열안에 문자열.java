91da00f95a295730bfa6fa767dde5aebd02f76fc
한승록 <127282898+shl2587@users.noreply.github.com>
2023-05-28 23:08:27 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 1;
+        if (!str1.contains(str2)) {
+            return 2;
+        }
+        return answer;
+    }
+}
