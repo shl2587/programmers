7da8de5fcd1c651adc53073fe0b348027b1101ee
한승록 <127282898+shl2587@users.noreply.github.com>
2023-06-01 15:10:11 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        int num = Integer.parseInt(n_str);  // 정수로 변환 시켜 0을 제거한 뒤
+        
+        return String.valueOf(num); // 해당 값을 문자열롤 변환
+    }
+}
