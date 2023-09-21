05de72ce377a6099d0a90994ac9fc402db5a341f
한승록 <127282898+shl2587@users.noreply.github.com>
2023-09-22 02:30:26 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] str_list) {
+        ArrayList<String> list = new ArrayList<> ();
+        
+        for (int i = 0; i < str_list.length; i++) {
+
+            if (str_list[i].equals("l")) {
+                for (int j = 0; j < i; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            }
+            else if (str_list[i].equals("r")) {
+                for (int j = i + 1; j < str_list.length; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            }
+        }
+        return list;
+    }
+}
