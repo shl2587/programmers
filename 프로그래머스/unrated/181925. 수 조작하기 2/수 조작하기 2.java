616c62e31fb29799ea3ae1ef8a75672fb2b3874c
한승록 <127282898+shl2587@users.noreply.github.com>
2023-10-08 19:06:27 +0900

    [unrated] Title: 수 조작하기 2, Time: 642.96 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,17 +2,17 @@ class Solution {
     public String solution(int[] numLog) {
         String answer = "";
         for (int i = 1; i < numLog.length; i++) {
-            if (numLog[i] == (numLog[i-1] + 1)) {
-                answer += 'w';
+            if (numLog[i] == (numLog[i-1] + 1)) { // 이전 숫자와 비교하여 1이 크면
+                answer += 'w';  // w를 덧셈
             }
-            else if(numLog[i] == (numLog[i-1] - 1)) {
-                answer += 's';
+            else if(numLog[i] == (numLog[i-1] - 1)) { // 이전 숫자와 비교하여 1이 작으면
+                answer += 's';  // s를 덧셈
             }
-            else if(numLog[i] == (numLog[i-1] + 10)) {
-                answer += 'd';
+            else if(numLog[i] == (numLog[i-1] + 10)) {  // 이전 숫자와 비교하여 10이 크면
+                answer += 'd';  // d를 덧셈
             }
-            else if(numLog[i] == (numLog[i-1] - 10)) {
-                answer += 'a';
+            else if(numLog[i] == (numLog[i-1] - 10)) {  // 이전 숫자와 비교하여 10이 작으면
+                answer += 'a';  // a를 덧셈
             }
         }
         return answer;
