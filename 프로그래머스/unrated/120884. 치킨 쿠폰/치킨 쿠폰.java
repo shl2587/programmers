2a12ba809a3c697b283c7ec76e76eb22d6b790e9
한승록 <127282898+shl2587@users.noreply.github.com>
2023-10-22 12:17:52 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.07 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int chicken) {
-        // 반복문 시작 시 1이 증가하므로 -1로 초기화
         int answer = 0;
         // chicken먹은 수만큼 반복하고 10씩 증가
         for (int i = 10; i <= chicken; i+=10) {
