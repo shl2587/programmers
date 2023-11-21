fcd5fc46dd915d4de0f558924a45cb8be2592e02
한승록 <127282898+shl2587@users.noreply.github.com>
2023-11-22 03:07:27 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT animal_id, name, sex_upon_intake
+    from animal_ins
+    where name = 'lucy' 
+        or name = 'ella' 
+        or name = 'pickle' 
+        or name = 'rogan'
+        or name = 'sabrina'
+        or name = 'mitty'
